993baa7d38d0951e4330da1c42d90a6dbfc05666
Reynaldo de Guzman <rdeguzman.itso@tip.edu.ph>
2021-11-08 16:19:36 +0800

    First Commit

---
+++ views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ratione quas quia officia perferendis eveniet error in explicabo voluptas expedita suscipit id nesciunt, aliquid, quam ipsa sapiente blanditiis voluptatum sequi repellat, libero eius qui cumque? Aperiam, consectetur nam, quod hic maiores similique ratione obcaecati dolores numquam autem rem soluta aliquam dolor provident commodi doloremque, tempore quisquam placeat sunt delectus eligendi quaerat voluptatibus facilis? Omnis accusantium magnam doloribus, excepturi nam porro quibusdam amet sequi dolore ut officiis doloremque ullam saepe totam ea ipsum voluptatibus tempora eius? Ratione aliquid nihil nulla molestiae quia id, error totam ipsa, vitae, possimus quibusdam? Iusto, quasi!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum iste quam odit ducimus nesciunt veritatis explicabo esse quis. In voluptatum laudantium obcaecati fugit, id odit deserunt temporibus neque natus laboriosam accusamus tenetur pariatur eius. Esse, doloremque, ex deserunt laudantium culpa possimus fugit quod, voluptatum animi maxime sed dolor sit iste optio quisquam praesentium repellendus soluta aspernatur. Dicta aspernatur maiores cupiditate voluptatem dignissimos asperiores? Minima, quo eum! Excepturi, sit praesentium non sequi aliquid dolorum repellat nulla cupiditate, cum necessitatibus modi hic dolorem veritatis eum earum vel fugit iusto soluta odio? Laudantium nobis accusamus, porro sapiente magnam tempora omnis. Esse, quod dolorum?</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
